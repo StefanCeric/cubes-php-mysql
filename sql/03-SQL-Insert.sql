c2bf35cfd92dc5b42e53db15d8ec6e5b0f3a0d04
Stefan Ceric <stefanceric88@gmail.com>
2017-10-30 16:12:52 +0100

    Cas 24 - kraj

--- sql/03-SQL-Insert.sql
+++ sql/03-SQL-Insert.sql
@@ -1,3 +1,16 @@
--- Zadatak: Napisati upit za unos jednog proizvoda
 
--- Zadatak: Napisati upit za unpos vise proizvoda
+
+-- Zadatak: Napisati upit za unos jednog proizvoda
+INSERT INTO products
+(brand, title, description, specification, price, quantity, category, on_sale, discount, created_at)
+VALUES
+('Apple', 'iPhone6 S', 'iPhone6 S', '', 39999.99, '12.0', 'Mobilni Telefon', 0, 0, '2016-03-21 18:00:00' );
+
+
+
+-- Zadatak: Napisati upit za unos vise proizvoda
+INSERT INTO products
+(brand, title, description, specification, price, quantity, category, on_sale, discount, created_at)
+VALUES
+('Apple', 'iPhone6 SE', 'iPhone6 SE', '', 29999.99, '10.0', 'Mobilni Telefon', 0, 0, '2016-03-21 18:00:00' ),
+('Apple', 'iPhone6 SE', 'iPhone5 ', '', 19999.99, '25.0', 'Mobilni Telefon', 0, 0, '2016-03-21 18:00:00' );
--- sql/05-SQL-Delete.sql
+++ sql/05-SQL-Delete.sql
@@ -1,5 +1,32 @@
+
 -- Napisati upit koji bise proizvod sa ID-jem 32
+DELETE
+FROM
+    products
+WHERE
+    id = 32;
+
 
 -- Napisati upit koji brise sve proizvode iz kategorije 'Ves masina'
+DELETE
+FROM
+    products
+WHERE
+    category = 'Ves Masina';
+
+
 
 -- Napisati upit koji brise proizvode sa ID-jevima 13, 23, 33
+DELETE
+FROM
+    products
+WHERE
+    id IN(13, 23, 33);
+
+
+-- CRUD - osnovne operacije u bazama:
+
+    --CREATE
+    --READ
+    --UPDATE
+    --DELETE
