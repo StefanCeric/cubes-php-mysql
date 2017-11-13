5e47b2c5271712240c41fa2515b20bf5fdd26a1a
Stefan <>
2017-11-13 01:32:11 +0100

    Uradjen crud-news

---
+++ crud-section-delete.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/models/m_users.php';
+
+if (!isUserLoggedIn()) {
+    header('Location: /login.php');
+    die();
+}
+
+require_once __DIR__ . '/models/m_groups.php';
+
+if (empty($_GET['id'])) {
+    die('Prosledi ID!');
+}
+
+$id = (int) $_GET['id'];
+
+$group = groupsFetchOneById($id);
+
+if (empty($group)) {
+    die('Trazena grupa ne postoji!');
+}
+
+if (isset($_POST["task"]) && $_POST["task"] == "delete") {
+    
+      groupsDeleteOneById($id);
+           
+      
+           header ('Location: /crud-group-list.php');
+           die();
+    
+}
+
+
+require_once __DIR__ .  '/views/layout/header.php';
+require_once __DIR__ .  '/views/templates/t_crud-group-delete.php';
+require_once __DIR__ .  '/views/layout/footer.php';
+
